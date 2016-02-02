d8e1f83c66be3d5a5e17c8dc077f0770f80d15ac
rinpopoyo <rinpopoyo@gmail.com>
2016-02-02 15:01:50 +0900

    3-4 try ( P12/12)

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -4,10 +4,13 @@ class TopController < ApplicationController
 
   def profile
     @user_name = "レイルズ"
-    @birth_day = "1987/6/10"
-    date_format = "%Y%m%d"
-    birthday = @birth_day.to_date
-    @age = (Date.today.strftime(date_format).to_i - birthday.strftime(date_format).to_i) / 10000
+    @birth_day = ""
+
+    unless @birth_day = ""  # unless @birth_day = blankだとエラーになる。 
+      date_format = "%Y%m%d"
+      birthday = @birth_day.to_date
+      @age = (Date.today.strftime(date_format).to_i - birthday.strftime(date_format).to_i) / 10000
+    end
     @hometown = "滋賀県"
     @skills = ["プログラミング", "詩の朗読", "ものまね"]
     @remarks = "よろしくお願い致します"
