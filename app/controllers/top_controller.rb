086363319c6ec5449a04ee7f2834e1961f3d544f
rinpopoyo <rinpopoyo@gmail.com>
2016-02-02 14:20:04 +0900

    3-2

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -5,7 +5,9 @@ class TopController < ApplicationController
   def profile
     @user_name = "レイルズ"
     @birth_day = "1987/6/10"
-    @age = "28"
+    date_format = "%Y%m%d"
+    birthday = @birth_day.to_date
+    @age = (Date.today.strftime(date_format).to_i - birthday.strftime(date_format).to_i) / 10000
     @hometown = "滋賀県"
     @skills = "プログラミング"
     @remarks = "よろしくお願い致します"
