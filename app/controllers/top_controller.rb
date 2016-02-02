7ef0bed05c5911432d8087c22fb2ec5d9ec8f0e3
rinpopoyo <rinpopoyo@gmail.com>
2016-02-02 14:07:02 +0900

    3-1

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -3,7 +3,12 @@ class TopController < ApplicationController
   end
 
   def profile
-
+    @user_name = "レイルズ"
+    @birth_day = "1987/6/10"
+    @age = "28"
+    @hometown = "滋賀県"
+    @skills = "プログラミング"
+    @remarks = "よろしくお願い致します"
   end
 
   def excercise
