ca3f4842c53460de2070e9655629be2d6c36e397
rinpopoyo <rinpopoyo@gmail.com>
2016-02-03 11:08:54 +0900

    5-1

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ActiveRecord::Base
+  enum gender: { unknown: 0, male: 1, female: 2, other: 9 }
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root "top#index"
   get "top/profile"
   get "top/excercise"
+  resources :users
 end
