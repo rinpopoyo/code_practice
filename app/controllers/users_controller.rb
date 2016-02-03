93c5447c541ac8a956bdc1070172fe7657f92692
rinpopoyo <rinpopoyo@gmail.com>
2016-02-04 02:06:10 +0900

    5-2 try & 5-3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,11 @@ class UsersController < ApplicationController
   # データを閲覧する画面を表示するためのAction
   def show
     @user = User.find(params[:id])
+    #unless @user.birthday = ""  # unless @birth_day = blankだとエラーになる。
+      date_format = "%Y%m%d"
+      birthday = @user.birthday.to_date
+      @age = (Date.today.strftime(date_format).to_i - birthday.strftime(date_format).to_i) / 10000
+    #end
   end
 
   # データを作成する画面を表示するためのAction
@@ -16,6 +21,7 @@ class UsersController < ApplicationController
 
   # データを更新する画面を表示するためのAction
   def edit
+    @user=User.find(params[:id])
   end
 
   # データを作成するためのAction
@@ -27,6 +33,9 @@ class UsersController < ApplicationController
 
   # データを更新するためのAction
   def update
+    @user = User.find(params[:id])
+    @user.update_attributes(user_params)
+    render :show
   end
 
   # データを削除するためのAction
